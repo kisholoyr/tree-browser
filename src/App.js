8c46a558b4a229065ad0f6603b3bbeb6ce74f2bf
kisholoyr <kisholoy@capitalnumbers.com>
2022-08-17 16:41:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import "./index.css";
 
-function App() {
+const StyledTree = styled.div`
+  line-height: 1.5;
+`;
+const StyledFile = styled.div`
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: 5px;
+  }
+`;
+const StyledFolder = styled.div`
+  padding-left: 20px;
+
+  .folder--label {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 5px;
+    }
+  }
+`;
+const Collapsible = styled.div`
+  height: ${(p) => (p.isOpen ? "0" : "auto")};
+  overflow: hidden;
+`;
+
+const File = ({ name }) => {
+  let ext = name.split(".")[1];
+
+  return (
+    <StyledFile>
+      <span>{name}</span>
+    </StyledFile>
+  );
+};
+
+const Folder = ({ name, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <StyledFolder>
+      <div className="folder--label" onClick={handleToggle}>
+        <span>{name}</span>
+      </div>
+      <Collapsible isOpen={isOpen}>{children}</Collapsible>
+    </StyledFolder>
+  );
+};
+
+const Tree = ({ children }) => {
+  return <StyledTree>{children}</StyledTree>;
+};
+
+Tree.File = File;
+Tree.Folder = Folder;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tree>
+        <Tree.Folder name="root">
+          <Tree.Folder name="directory-1">
+            <Tree.File name="Modal.js" />
+            <Tree.File name="Modal.css" />
+          </Tree.Folder>
+          <Tree.Folder name="directory-2">
+            <Tree.File name="Modal.js" />
+            <Tree.File name="Modal.css" />
+          </Tree.Folder>
+          <Tree.Folder name="directory-3">
+            <Tree.File name="Modal.js" />
+            <Tree.File name="Modal.css" />
+          </Tree.Folder>
+        </Tree.Folder>
+        <Tree.File name="index.js" />
+        <Tree.File name="component-1.js" />
+        <Tree.File name="component-2.js" />
+      </Tree>
     </div>
   );
 }
-
-export default App;
